720fc5540197ddc6b7746aaa8529ef14eace6b7b
Mark Shelgren <markshelgren@outlook.com>
2021-02-03 14:41:06 -0800

    resolving github pages

---
+++ build/precache-manifest.ffc54a976afab86dcc1ef5ade091c1a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c8c727d0fe24e41fdca4718090d683",
+    "url": "/markshelgren/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "0f15e65dfbfa7c86c830",
+    "url": "/markshelgren/EmployeeDirectory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c59cbff2334d3fa1064d",
+    "url": "/markshelgren/EmployeeDirectory/static/js/2.2ba8b590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markshelgren/EmployeeDirectory/static/js/2.2ba8b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f15e65dfbfa7c86c830",
+    "url": "/markshelgren/EmployeeDirectory/static/js/main.b01f4f3b.chunk.js"
+  },
+  {
+    "revision": "a61276a8169d8a078046",
+    "url": "/markshelgren/EmployeeDirectory/static/js/runtime-main.e68f9a7e.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//markshelgren.github.io/EmployeeDirectory/precache-manifest.6d11a707e435336989763fe457e0157e.js"
+  "/markshelgren/EmployeeDirectory/precache-manifest.ffc54a976afab86dcc1ef5ade091c1a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//markshelgren.github.io/EmployeeDirectory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/markshelgren/EmployeeDirectory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
